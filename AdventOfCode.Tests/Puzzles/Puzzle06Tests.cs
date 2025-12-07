086cc374a8e1c7026308ac5f819548dc89b63f05
Julian Klock <julian.klock@gmail.com>
2025-12-07 09:56:00 +0100

    Solve puzzle 6 part 2

--- AdventOfCode.Tests/Puzzles/Puzzle06Tests.cs
+++ AdventOfCode.Tests/Puzzles/Puzzle06Tests.cs
@@ -16,11 +16,11 @@ public class Puzzle06Tests
         result.Should().Be(4878670269096);
     }
 
-    // [Fact(Skip = "Not yet implemented")]
+    [Fact]
     public void SolvePart2()
     {
         var result = _puzzle.SolvePart2();
-        result.Should().Be(0);
+        result.Should().Be(8674740488592);
     }
 
     [Fact]
@@ -34,5 +34,21 @@ public class Puzzle06Tests
         );
         var result = puzzle.SolvePart1();
         result.Should().Be(4277556);
+    }
+    
+    // 123 328  51 64 
+    //  45 64  387 23 
+    //   6 98  215 314
+    // *   +   *   +  
+    [Theory]
+    [InlineData(new[] { "64 ", "23 ", "314" }, new long[] { 4, 431, 623 })]
+    [InlineData(new[] { " 51", "387", "215" }, new long[] { 175, 581, 32 })]
+    [InlineData(new[] { "328", "64 ", "98 " }, new long[] { 8, 248, 369 })]
+    [InlineData(new[] { "123", " 45", "  6" }, new long[] { 356, 24, 1 })]
+    
+    public void TestPart2Parsing(string[] input, long[] parsedInput)
+    {
+        var result = Puzzle06.ParseInputPart2(input);
+        result.Should().BeEquivalentTo(parsedInput);
     }
 }
--- AdventOfCode/Puzzles/Puzzle06.cs
+++ AdventOfCode/Puzzles/Puzzle06.cs
@@ -10,7 +10,7 @@ public class Puzzle06 : Puzzle<string[], long>
 
     public override long SolvePart1()
     {
-        return CalculateGrandTotal(ParseInputPart1);
+        return CalculateGrandTotal(InputEntries.Select(ie => ie.ToList()).ToList(), ParseInputPart1);
     }
 
     private static IEnumerable<long> ParseInputPart1(IEnumerable<string> problemInput)
@@ -20,25 +20,67 @@ public class Puzzle06 : Puzzle<string[], long>
 
     public override long SolvePart2()
     {
-        throw new NotImplementedException();
+        // For parsing part 2, each entry is a single line
+        // Add an extra space at the end, for easier logic below
+        var inputLines = InputEntries.Select(ie => ie[0] + " ").ToList();
+        
+        var parsedLines = new List<List<string>>();
+        for (var i = 0; i < inputLines.Count; i++)
+        {
+            parsedLines.Add([]);
+        }
+        
+        var previousIndex = 0;
+        var currentIndex = 0;
+        // Must do a bit more advanced splitting of each input line to keep all whitespace.
+        // A space at the same position on every line indicates a new number.
+        while (currentIndex < inputLines[0].Length)
+        {
+            if (inputLines.All(il => il[currentIndex] == ' '))
+            {
+                var column = inputLines.Select(il => il[previousIndex..currentIndex]).ToList();
+                for (var i = 0; i < column.Count; i++)
+                {
+                    var row = column[i];
+                    parsedLines[i].Add(row);
+                }
+
+                previousIndex = currentIndex + 1;
+            }
+
+            currentIndex++;
+        }
+        return CalculateGrandTotal(parsedLines, ParseInputPart2);
     }
 
-    private long CalculateGrandTotal(Func<IEnumerable<string>, IEnumerable<long>> parseInput)
+    public static IEnumerable<long> ParseInputPart2(IEnumerable<string> problemInput)
+    {
+        var numbers = new List<string>();
+        var column = problemInput.ToList();
+        for (var i = 0; i < column[0].Length; i++)
+        {
+            var number = column.Select(c => c[i]).CreateString().Trim();
+            numbers.Add(number);
+        }
+        return numbers.Select(long.Parse);
+    }
+
+    private static long CalculateGrandTotal(List<List<string>> inputEntries, Func<IEnumerable<string>, IEnumerable<long>> parseInput)
     {
         var grandTotal = 0L;
 
-        var problemInputCount = InputEntries.Count - 1;
-        for (var i = 0; i < InputEntries[0].Length; i++)
+        var problemInputCount = inputEntries.Count - 1;
+        for (var i = 0; i < inputEntries[0].Count; i++)
         {
-            var problemOperator = InputEntries[problemInputCount][i]; // The operator is the last row of input items
-            var problemInput = InputEntries
+            var problemOperator = inputEntries[problemInputCount][i]; // The operator is the last row of input items
+            var problemInput = inputEntries
                 .Select(row => row[i])
                 .Take(problemInputCount);
             var parsedInput = parseInput(problemInput)
                 .ToArray();
             
             long problemTotal;
-            if (problemOperator == "+")
+            if (problemOperator.Trim() == "+")
             {
                 problemTotal = parsedInput.Sum();
             }
@@ -57,5 +99,13 @@ public class Puzzle06 : Puzzle<string[], long>
     {
         var strings = inputItem.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
         return strings;
+    }
+
+    protected internal override string[] ParseAlternateInput(string inputItem)
+    {
+        // Parsing the input for part 2 requires all lines to bo considered at the same time.
+        // We therefore just have to return a single-entry array here, and then we'll to the
+        // actual parsing in SolvePart2
+        return [inputItem];
     }
 }
