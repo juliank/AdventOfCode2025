ae23384043bd922ad57c2000d365ebbcae573053
Julian Klock <julian.klock@gmail.com>
2025-12-10 23:26:02 +0100

    Add IsSurroundedBy method to Point

---
+++ AdventOfCode.Tests/Models/PointTests.cs
@@ -0,0 +1,71 @@
+﻿// ReSharper disable GrammarMistakeInComment
+namespace AdventOfCode.Tests.Models;
+
+public class PointTests
+{
+    [Theory]
+    // Points along the horizontal line through the middle of the polygon
+    [InlineData(0, 2, false)]
+    [InlineData(1, 2, true)]
+    [InlineData(2, 2, true)]
+    [InlineData(3, 2, true)]
+    [InlineData(4, 2, false)]
+    // Points that are fully above/below the polygon
+    [InlineData(2, 0, false)]
+    [InlineData(2, 4, false)]
+    // Points that start on the same horizontal line as the top/bottom of the polygon
+    [InlineData(4, 1, false)]
+    [InlineData(4, 3, false)]
+    public void IsSurroundedBy_ForPolygon1_ReturnsExpected(int x, int y, bool expected)
+    {
+        // Create a polygon consisting of a simple square:
+        //
+        // Y\X 0 1 2 3 4 X
+        //  0 
+        //  1    . . .
+        //  2    .   .
+        //  3    . . .
+        //  4
+        //  Y
+        List<Point> polygon =
+            [
+                new(1, 1), new(2, 1), new(3, 1),
+                new(1, 2),            new(3, 2),
+                new(1, 3), new(2, 3), new(3, 3)
+            ];
+        var isSurroundedBy = new Point(x, y).IsSurroundedBy(polygon);
+        isSurroundedBy.Should().Be(expected);
+    }
+    
+    [Theory]
+    // Simple alternatives with only "direct" crossings of the polygon
+    [InlineData(0, 3, false)]
+    [InlineData(1, 3, true)]
+    [InlineData(3, 3, true)]
+    [InlineData(5, 3, true)]
+    [InlineData(6, 3, false)]
+    // Point outside, but leftwards ray tracing will partially traverse the polygon (from (5,2) to (3,2)
+    [InlineData(6, 2, false)]
+    public void IsSurroundedBy_ForPolygon2_ReturnsExpected(int x, int y, bool expected)
+    {
+        // Create a polygon consisting of a simple square:
+        //
+        // Y\X 0 1 2 3 4 5 6 X
+        //  0 
+        //  1    . . .
+        //  2    .   . . .
+        //  3    .       .
+        //  4    . . . . .
+        //  5
+        //  Y
+        List<Point> polygon =
+        [
+            new(1, 1), new(2, 1), new(3, 1),
+            new(1, 2),            new(3, 2), new(4, 2), new(5, 2),
+            new(1, 3),                                  new(5, 3),
+            new(1, 4), new(2, 4), new(3, 4), new(4, 4), new(5, 4)
+        ];
+        var isSurroundedBy = new Point(x, y).IsSurroundedBy(polygon);
+        isSurroundedBy.Should().Be(expected);
+    }
+}
--- AdventOfCode/Models/Point.cs
+++ AdventOfCode/Models/Point.cs
@@ -56,6 +56,12 @@ public readonly record struct Point(int X, int Y, int Z = 0)
         throw new NotImplementedException($"Missing support to find direction from {this} to {point}");
     }
 
+    /// <summary>
+    /// Check if this point is within the given boundary.
+    ///
+    /// Being *on* the boundary counts as being within.
+    /// </summary>
+    /// <remarks>Not to be confused with <see cref="IsSurroundedBy"/></remarks>
     public bool IsWithin(Boundary boundary)
     {
         // Comparing against a nullable that is null will always yield false,
@@ -139,5 +145,102 @@ public readonly record struct Point(int X, int Y, int Z = 0)
         var v2 = new Vector3(point.X, point.Y, point.Z);
         var distance = Vector3.Distance(v1, v2);
         return distance;
+    }
+
+    /// <summary>
+    /// Deciding if the point is within a (continuous, and presumably closed) polygon.
+    ///
+    /// Based on: https://www.eecs.umich.edu/courses/eecs380/HANDOUTS/PROJ2/InsidePoly.html
+    /// To determine the status of a point (xp,yp) consider a horizontal ray emanating from (xp,yp) and to the right.
+    /// If the number of times this ray intersects the line segments making up the polygon is even then the point is
+    /// outside the polygon. Whereas if the number of intersections is odd then the point (xp,yp) lies inside the polygon.
+    /// </summary>
+    /// <remarks>Not to be confused with <see cref="IsWithin"/></remarks>
+    public bool IsSurroundedBy(List<Point> polygon)
+    {
+        if (Z != 0)
+        {
+            throw new NotSupportedException($"Currently only supported for 2D points (this point has Z = {Z})");
+        }
+        
+        // Boundary holding the min and max values for X and Y of the polygon,
+        // so we know how far we must do the ray tracing.
+        var boundary = new Boundary(
+            polygon.MinBy(p => p.X).X - 1, polygon.MinBy(p => p.Y).Y - 1,
+            polygon.MaxBy(p => p.X).X + 1, polygon.MaxBy(p => p.Y).Y + 1);
+        
+        // We do a simple ray tracing starting towards the left, until we're outside the boundary
+        var currentPoint = new Point(X, Y);
+        if (!currentPoint.IsWithin(boundary))
+        {
+            return false;
+        }
+
+        if (polygon.Contains(currentPoint))
+        {
+            // If the point itself is actually *on* the polygon, we do an early return.
+            // Both for simplicity but also because that could mess up the odd/even
+            // logic in the algorithm below.
+            return true;
+        }
+
+        var polygonCrossings = 0;
+
+        var previousOnPolygon = false;
+        var enteredAtUpwardsAngle = false;
+        var enteredAtDownwardsAngle = false;
+        
+        while (currentPoint.IsWithin(boundary))
+        {
+            var northPoint = currentPoint.GetN();
+            var southPoint = currentPoint.GetS();
+            if (polygon.Contains(currentPoint) && polygon.Contains(northPoint) &&  polygon.Contains(southPoint))
+            {
+                // We're crossing a vertical piece of the polygon
+                polygonCrossings++;
+            }
+            else if(polygon.Contains(currentPoint))
+            {
+                if (!previousOnPolygon)
+                {
+                    // We've entered a horizontal piece of the polygon ...
+                    previousOnPolygon = true;
+                    // ... and must keep track of the angle at the entering point
+                    if (polygon.Contains(northPoint))
+                    {
+                        enteredAtUpwardsAngle = true;
+                    }
+                    else if (polygon.Contains(southPoint))
+                    {
+                        enteredAtDownwardsAngle = true;
+                    }
+                }
+                
+                var nextPoint = currentPoint.GetW();
+                if (!polygon.Contains(nextPoint))
+                {
+                    // On the next point of the ray trace we'll be leaving the polygon again
+                    if ((polygon.Contains(northPoint) && enteredAtDownwardsAngle) ||
+                        (polygon.Contains(northPoint) && enteredAtUpwardsAngle))
+                    {
+                        // Leaving at an "opposite" angle as the one we entered at means that
+                        // we have crossed the polygon
+                        polygonCrossings++;
+                    }
+
+                    enteredAtUpwardsAngle = false;
+                    enteredAtDownwardsAngle = false;
+                }
+            }
+            else
+            {
+                previousOnPolygon = false;
+            }
+
+            currentPoint = currentPoint.GetW();
+        }
+        
+        var isOutsidePolygon = polygonCrossings % 2 == 0;
+        return !isOutsidePolygon;
     }
 }
